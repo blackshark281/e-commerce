fc765437fbab0275addfd86f1e88cc802bd2b6ee
blackshark281 <79707557+blackshark281@users.noreply.github.com>
2022-01-22 22:40:32 +0530

    Add files via upload

---
+++ client/src/components/Products.jsx
@@ -0,0 +1,73 @@
+import styled from "styled-components";
+//import { popularProducts } from "../data";
+import Product from "./Product";
+import { useEffect, useState } from "react";
+import axios from "axios";
+//import { axiosInstance } from "../config";
+
+const Container = styled.div`
+    padding: 20px;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+`;
+
+const Products = ({cat, filters, sort}) => {
+ // console.log(cat, filters, sort)               testing
+
+  const [products,setProducts] = useState([]);
+  const [filteredProducts, setFilteredProducts] = useState([]);
+
+  useEffect(() => {
+    const getProducts = async ()=>{
+      try{
+        const res = await axios.get(cat ? `http://localhost:5000/api/products?category=${cat}` :
+                                          "http://localhost:5000/api/products");
+       // console.log(res);      // for testing
+      // console.log(res.data);     // for testing
+      setProducts(res.data);                        // WARNING LINE
+      }catch(err){}
+    };
+    getProducts();
+  }, [cat]);
+
+  useEffect(() => {
+    cat && setFilteredProducts(
+      products.filter((item) => 
+        Object.entries(filters).every(([key,value])=>
+          item[key].includes(value)
+        )
+      )
+    );
+  }, [products,cat,filters]);
+  
+  //console.log(cat);
+
+  useEffect(() => {
+    if (sort === "newest") {
+      setFilteredProducts((prev) =>
+        [...prev].sort((a, b) => a.createdAt - b.createdAt)
+      );
+    } else if (sort === "asc") {
+      setFilteredProducts((prev) =>
+        [...prev].sort((a, b) => a.price - b.price)
+      );
+    } else {
+      setFilteredProducts((prev) =>
+        [...prev].sort((a, b) => b.price - a.price)
+      );
+    }
+  }, [sort]);
+
+  return (
+    <Container>
+      {cat
+        ? filteredProducts.map((item) => <Product item={item} key={item.id} />)
+        : products
+            .slice(0, 8)
+            .map((item) => <Product item={item} key={item.id} />)}
+    </Container>
+  );
+};
+
+export default Products;
